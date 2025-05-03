c37e17a27d72c5e5cb44246b13cf3cb770a62862
bshasikiran <133859438+bshasikiran@users.noreply.github.com>
2025-05-04 01:22:19 +0530

    Update Signup.tsx

--- frontend-react/src/pages/Signup.tsx
+++ frontend-react/src/pages/Signup.tsx
@@ -29,7 +29,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:7000/auth/register', {
+      const response = await fetch('https://mini-project-cmrtc-api.onrender.com/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
